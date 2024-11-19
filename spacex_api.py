5f26b139c2e44cd22ac4403a7dc3c3a7b1d7f487
Hari Gopal Matta <harigopalmatta>
2024-11-19 16:08:38 +0800

    get api data using pandas

--- spacex_api.py
+++ spacex_api.py
@@ -1,9 +1,13 @@
 import requests
 import pandas as pd
 
-response = requests.get("https://api.spacexdata.com/v5/launches")
-launch_data = response.json()
+url = "https://api.spacexdata.com/v5/launches"
+launch_data = pd.read_json(url)
 
 launch_df = pd.DataFrame(launch_data)
 
-print(launch_df.head(5))
+print(launch_df['rocket'])
+print(launch_df['name'])
+print(launch_df['flight_number'])
+print(launch_df['success'])
+print(launch_df.dtypes)
